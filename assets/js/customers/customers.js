e6c9314a8b54cfb1a7a15c03e539a55ffad640a8
Rawezh0894 <rawezh.soft94@gmail.com>
2025-05-20 19:30:26 +0300

    Fix due_date handling in transactions

--- assets/js/customers/customers.js
+++ assets/js/customers/customers.js
@@ -208,7 +208,7 @@ function renderPagination(totalPages) {
     const prevDisabled = currentPage <= 1 ? 'disabled' : '';
     paginationHtml += `
     <li class="page-item ${prevDisabled}">
-        <a class="page-link rounded-circle mx-1" href="javascript:void(0)" data-page="${currentPage - 1}" aria-label="Previous">
+        <a class="page-link rounded-circle mx-1" href="#" data-page="${currentPage - 1}" aria-label="Previous">
             <span aria-hidden="true">&laquo;</span>
         </a>
     </li>
@@ -231,7 +231,7 @@ function renderPagination(totalPages) {
     if (startPage > 1) {
         paginationHtml += `
         <li class="page-item">
-            <a class="page-link rounded-circle mx-1" href="javascript:void(0)" data-page="1">1</a>
+            <a class="page-link rounded-circle mx-1" href="#" data-page="1">1</a>
         </li>
         `;
         
@@ -250,7 +250,7 @@ function renderPagination(totalPages) {
         const active = i === currentPage ? 'active' : '';
         paginationHtml += `
         <li class="page-item ${active}">
-            <a class="page-link rounded-circle mx-1" href="javascript:void(0)" data-page="${i}">${i}</a>
+            <a class="page-link rounded-circle mx-1" href="#" data-page="${i}">${i}</a>
         </li>
         `;
     }
@@ -268,7 +268,7 @@ function renderPagination(totalPages) {
         
         paginationHtml += `
         <li class="page-item">
-            <a class="page-link rounded-circle mx-1" href="javascript:void(0)" data-page="${totalPages}">${totalPages}</a>
+            <a class="page-link rounded-circle mx-1" href="#" data-page="${totalPages}">${totalPages}</a>
         </li>
         `;
     }
@@ -277,7 +277,7 @@ function renderPagination(totalPages) {
     const nextDisabled = currentPage >= totalPages ? 'disabled' : '';
     paginationHtml += `
     <li class="page-item ${nextDisabled}">
-        <a class="page-link rounded-circle mx-1" href="javascript:void(0)" data-page="${currentPage + 1}" aria-label="Next">
+        <a class="page-link rounded-circle mx-1" href="#" data-page="${currentPage + 1}" aria-label="Next">
             <span aria-hidden="true">&raquo;</span>
         </a>
     </li>
@@ -286,6 +286,18 @@ function renderPagination(totalPages) {
     paginationHtml += '</ul></nav>';
     
     pagination.innerHTML = paginationHtml;
+
+    // Add click event listeners to pagination links
+    const paginationLinks = pagination.querySelectorAll('.page-link');
+    paginationLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const page = this.getAttribute('data-page');
+            if (page) {
+                changePage(parseInt(page));
+            }
+        });
+    });
 }
 
 // Function to change page
--- assets/js/suppliers/suppliers.js
+++ assets/js/suppliers/suppliers.js
@@ -180,7 +180,7 @@ function renderPagination(totalPages) {
     const prevDisabled = currentPage <= 1 ? 'disabled' : '';
     paginationHtml += `
     <li class="page-item ${prevDisabled}">
-        <a class="page-link rounded-circle mx-1" href="javascript:void(0)" data-page="${currentPage - 1}" aria-label="Previous">
+        <a class="page-link rounded-circle mx-1" href="#" data-page="${currentPage - 1}" aria-label="Previous">
             <span aria-hidden="true">&laquo;</span>
         </a>
     </li>
@@ -203,7 +203,7 @@ function renderPagination(totalPages) {
     if (startPage > 1) {
         paginationHtml += `
         <li class="page-item">
-            <a class="page-link rounded-circle mx-1" href="javascript:void(0)" data-page="1">1</a>
+            <a class="page-link rounded-circle mx-1" href="#" data-page="1">1</a>
         </li>
         `;
         
@@ -222,7 +222,7 @@ function renderPagination(totalPages) {
         const active = i === currentPage ? 'active' : '';
         paginationHtml += `
         <li class="page-item ${active}">
-            <a class="page-link rounded-circle mx-1" href="javascript:void(0)" data-page="${i}">${i}</a>
+            <a class="page-link rounded-circle mx-1" href="#" data-page="${i}">${i}</a>
         </li>
         `;
     }
@@ -240,7 +240,7 @@ function renderPagination(totalPages) {
         
         paginationHtml += `
         <li class="page-item">
-            <a class="page-link rounded-circle mx-1" href="javascript:void(0)" data-page="${totalPages}">${totalPages}</a>
+            <a class="page-link rounded-circle mx-1" href="#" data-page="${totalPages}">${totalPages}</a>
         </li>
         `;
     }
@@ -249,7 +249,7 @@ function renderPagination(totalPages) {
     const nextDisabled = currentPage >= totalPages ? 'disabled' : '';
     paginationHtml += `
     <li class="page-item ${nextDisabled}">
-        <a class="page-link rounded-circle mx-1" href="javascript:void(0)" data-page="${currentPage + 1}" aria-label="Next">
+        <a class="page-link rounded-circle mx-1" href="#" data-page="${currentPage + 1}" aria-label="Next">
             <span aria-hidden="true">&raquo;</span>
         </a>
     </li>
@@ -258,6 +258,18 @@ function renderPagination(totalPages) {
     paginationHtml += '</ul></nav>';
     
     pagination.innerHTML = paginationHtml;
+
+    // Add click event listeners to pagination links
+    const paginationLinks = pagination.querySelectorAll('.page-link');
+    paginationLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const page = this.getAttribute('data-page');
+            if (page) {
+                changePage(parseInt(page));
+            }
+        });
+    });
 }
 
 // Function to change page
